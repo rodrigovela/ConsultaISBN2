cbdf2f41eeceb603030cc97b005a45fede3c6d20
Roy <rodrigovelazquez@macbook-pro-de-rodrigo.local>
2016-09-05 00:04:34 -0500

    Upload

--- Consulta/ViewController.swift
+++ Consulta/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var infoTextView: UITextView!
+    
+    @IBOutlet weak var autorLabel: UILabel!
+    @IBOutlet weak var tituloLabel: UILabel!
     @IBOutlet weak var isbnTextField: UITextField!
     
-    func searchBook(urls: String){
-        let url = NSURL(string: urls)
-        let datos:NSData? = NSData(contentsOfURL: url!)
-        let text = NSString(data: datos!, encoding: NSUTF8StringEncoding)
-        print(text!)
-        infoTextView.text = text! as String
-    }
+    @IBOutlet weak var portada: UIImageView!
+    
+    
     
     func searchBookAsynch(urls: String) {
         
@@ -32,7 +30,51 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 
                                 if((resp) != nil){
                                     dispatch_async(dispatch_get_main_queue()) {
-                                        self.infoTextView.text = text! as String
+                                        //self.infoTextView.text = text! as String
+                                        
+                                        do{
+                                            
+                                            let isbn = self.isbnTextField.text! as String
+                                            let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                                            let dic1 = json as! NSDictionary
+                                            let isbn2 = "ISBN:\(isbn)"
+                                            if((dic1[isbn2]) != nil){
+                                                let dic2 = dic1[isbn2] as! NSDictionary
+                                                self.tituloLabel.text = dic2["title"] as! NSString as String
+                                                let dic3 = dic2["authors"] as! NSArray
+                                                for autor in dic3
+                                                {
+                                                    let dic4 = autor as! NSDictionary
+                                                    let str = dic4["name"] as! NSString as String
+                                                    if((self.autorLabel.text) != nil){
+                                                        self.autorLabel.text = self.autorLabel.text! + "; " + str
+                                                    }
+                                                    else{
+                                                        self.autorLabel.text = str
+                                                    }
+                                                }
+                                        
+                                                if((dic2["cover"]) != nil)
+                                                {
+                                                    let dic5 = dic2["cover"] as! NSDictionary
+                                                    let imgURL = dic5["large"] as! NSString as String
+                                                    dispatch_async(dispatch_get_main_queue()) {
+                                                        if let iURL = NSURL(string: imgURL) {
+                                                            if let data = NSData(contentsOfURL: iURL) {
+                                                                self.portada.image = UIImage(data: data)
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            else{
+                                                dispatch_async(dispatch_get_main_queue()) {
+                                                    self.displayMyAlertMessage("No encontrado")
+                                                }
+                                                return
+                                            }
+                                        }
+                                        catch _ {}
                                     }
                                 }
                                 else {
@@ -57,7 +99,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.isbnTextField.delegate = self
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -88,8 +129,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func clearTextView(sender: UITextField){
-        if((infoTextView.text) != nil){
-            infoTextView.text = nil
+        if((tituloLabel.text) != nil || (autorLabel.text) != nil){
+            tituloLabel.text = nil
+            autorLabel.text = nil
+            portada.image = nil
         }
     }
     
